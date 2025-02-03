b74219dc39b66219761184d7d26e862ca61630ef
Kris Norful <krisnorful@gmail.com>
2025-02-03 12:32:16 -0600

    feat: Implement basic weather fetching functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const API_KEY = "d708730abb785ed356340497f9b0296e";
+const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 function App() {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("Bentonville");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchWeather() {
+      try {
+        const response = await axios.get(
+          `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`
+        );
+        console.log(response.data);
+        setWeather(response.data);
+        setError(null);
+      } catch (e) {
+        setError("City not found. Try again..");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchWeather();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Weather App</h1>
+        <h2>City Name: {city}</h2>
+        <p>Weather: {weather.main.temp}</p>
+        <p>Condition: {weather.weather[0].description}</p>
       </header>
     </div>
   );
