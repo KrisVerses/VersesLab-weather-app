cbab834166e9d50b173f000941325ad5b5438772
Kris Norful <krisnorful@gmail.com>
2025-02-03 16:50:17 -0600

    refactor: extract API KEY and place into .env file

--- src/App.js
+++ src/App.js
@@ -2,37 +2,57 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import WeatherDisplay from "./components/WeatherDisplay";
 
-const API_KEY = "d708730abb785ed356340497f9b0296e";
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 function App() {
   const [weather, setWeather] = useState(null);
-  const [city, setCity] = useState("Bentonville");
+  const [city, setCity] = useState("");
+  const [input, setInput] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    if (!city) return;
     async function fetchWeather() {
+      setLoading(true);
       try {
         const response = await axios.get(
           `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`
         );
-        console.log(response.data);
+        console.log(response);
         setWeather(response.data);
         setError(null);
       } catch (e) {
         setError("City not found. Try again..");
+        setWeather(null);
       } finally {
         setLoading(false);
       }
     }
     fetchWeather();
-  }, []);
+  }, [city]);
+
+  const handleInputChange = ({ target }) => setInput(() => target.value);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (!input.trim()) return;
+    setCity(() => input.trim());
+  };
 
   return (
     <div>
       <header>
         <h1>Weather App</h1>
+        <nav>
+          <input
+            value={input}
+            onChange={handleInputChange}
+            placeholder="Enter City"
+          />
+          <button onClick={handleSearch}>Search</button>
+        </nav>
       </header>
       {loading && <p>Loading...</p>}
       {error && <p>{error}</p>}
